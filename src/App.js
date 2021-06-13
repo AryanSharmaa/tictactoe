1fbd7bcdd551a89af50565bfa9deaad4108cd4b9
Aryan Sharma <aryansharma1606@gmail.com>
2021-06-13 16:54:42 +0530

    git init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+  <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </>
+);
+  };
+
+  export default App;
